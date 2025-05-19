d881820888473440e0b9d7c1356d36fb642c513a
sanjee1999 <sanjeevkanth1999@gmail.com>
2025-05-19 14:22:16 +0530

    #

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -4,10 +4,13 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Str;
 use App\Models\SiteSetting;
 
+
 if (!function_exists('is_join_form_enabled')) {
     function is_join_form_enabled()
     {
-        return SiteSetting::where('key', 'join_form_enabled')->value('value'); // returns the value directly
+        $value = SiteSetting::where('key', 'join_form_enabled')->value('value');
+
+        return !empty($value) ? $value : false;
     }
 }
 
